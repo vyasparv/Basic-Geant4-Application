c8747ae281462278442685872dfb789ec203bd78
Parv Vyas <94159199+vyasparv@users.noreply.github.com>
2024-01-21 12:53:21 +0530

    Add files via upload

---
+++ fisica.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+
+#include "G4RunManager.hh"
+#include "G4UImanager.hh"
+#include "G4VisManager.hh"
+#include "G4VisExecutive.hh"
+#include "G4UIExecutive.hh"
+
+#include "DetectorConstruction.hh"
+#include "PhysicsList.hh"
+#include "ActionInitialization.hh"
+
+int main(int argc, char** argv)
+{
+	G4RunManager runManager = new G4RunManager();
+
+	runManager->SetUserInitialization(new DetectorConstruction());
+	runManager->SetUserInitialization(new PhysicsList());
+	runManager->SetUserInitialization(new ActionInitialization());
+
+	runManager->Initialize();
+
+	G4UIExecutive ui = new G4UIExecutive(argc, argv);
+
+	G4VisManager visManager = new GVisExecutive();
+	visManager->Initialize();
+
+	G4UImanager UImanager = G4UImanager::GetUIpointer();
+
+	UImanager->ApplyCommand("/vis/open OGL");
+	UImanager->ApplyCommand("/vis/viewer/set/viewpointVector 1 1 1");
+	UImanager->ApplyCommand("/vis/drawVolume");
+	UImanager->ApplyCommand("/vis/viewer/set/autoRefresh true");
+	UImanager->ApplyCommand("/vis/scene/add/trajectories smooth");
+	UImanager->ApplyCommand("/vis/scene/endofEventAction accumulate");
+
+
+	ui->SessionStart();
+	
+	return 0;
+}
